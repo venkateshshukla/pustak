5257f600784b448cc5588ecfcf2b2bc2c3a92050
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2017-04-28 19:17:34 +0530

    Segregate the methods in the constructor. Make it testable.
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- src/main/java/in/vshukla/booksindia/DbConnection.java
+++ src/main/java/in/vshukla/booksindia/DbConnection.java
@@ -1,5 +1,6 @@
 package in.vshukla.booksindia;
 
+import in.vshukla.booksindia.exceptions.AppRuntimeException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,6 +9,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import static in.vshukla.booksindia.AppUtils.blankStringCheck;
 
 /**
  * This is a class following singleton pattern.
@@ -21,33 +24,63 @@ public class DbConnection {
 
     private Connection connection;
 
+    /**
+     * Made private for Singleton pattern.
+     */
     private DbConnection() {
+    }
 
+    /**
+     * Initialize the DB connection.
+     * Checks for presence of Driver as well.
+     */
+    private void initialize() {
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
             LOGGER.error("DB: Unable to find sql driver.", e);
-            throw new AppException("Unable to find sql driver", e);
+            throw new AppRuntimeException("Unable to find sql driver", e);
         }
 
-        String url = AppUtils.getProperty(AppConstants.PROP_DB_URL);
-        if (url == null) {
-            String user = System.getenv(AppConstants.PROP_USER);
-            url = String.format(AppConstants.DEFAULT_DB_URL, user);
-        }
+        DbCred cred = getDbCred();
+        this.connection = getConnection(cred);
+    }
 
-        String user = AppUtils.getProperty(AppConstants.PROP_DB_USR);
-        String pwd = AppUtils.getProperty(AppConstants.PROP_DB_PWD);
-
-        LOGGER.debug("DB: Attempting connection to URL {} with USER {} and PWD {}", url, user, pwd);
+    /**
+     * Create a connection using the given Database credentials.
+     *
+     * @param cred  Database credentials
+     * @return
+     */
+    private Connection getConnection(DbCred cred) {
+        LOGGER.debug("DB: Attempting connection using {}", cred);
+        Connection connection = null;
         try {
-            connection = DriverManager.getConnection(url, user, pwd);
+            connection = DriverManager.getConnection(cred.url, cred.user, cred.pwd);
             connection.setAutoCommit(false);
         } catch (SQLException e) {
             LOGGER.error("DB: Unable to open SQL Connection", e);
-            throw new AppException("Unable to open SQL Connection.", e);
+            throw new AppRuntimeException("Unable to open SQL Connection.", e);
         }
         LOGGER.info("DB: Connection successful.");
+        return connection;
+    }
+
+    /**
+     * Create a {@link DbCred} instance using environmental variables.
+     * The variables used are :
+     *
+     * URL = {@value AppConstants#PROP_DB_URL}; Default value = {@value AppConstants#DEFAULT_DB_URL} using Environment variable {@value AppConstants#PROP_USER}
+     * USER = {@value AppConstants#PROP_DB_USR}; Default value = Environment variable {@value AppConstants#PROP_USER}
+     * PASSWD = {@value AppConstants#PROP_DB_PWD}; Default value = {@value AppConstants#DEFAULT_DB_PWD}
+     *
+     * @return {@link DbCred} object containing the Database connection parameters.
+     */
+    private DbCred getDbCred() {
+        String url = AppUtils.getProperty(AppConstants.PROP_DB_URL, () -> String.format(AppConstants.DEFAULT_DB_URL, System.getenv(AppConstants.PROP_USER)));
+        String user = AppUtils.getProperty(AppConstants.PROP_DB_USR, () -> System.getenv(AppConstants.PROP_USER));
+        String pwd = AppUtils.getProperty(AppConstants.PROP_DB_PWD, () -> AppConstants.DEFAULT_DB_PWD);
+        return new DbCred(url, user, pwd);
     }
 
     /**
@@ -75,6 +108,15 @@ public class DbConnection {
     }
 
     /**
+     * Execute the given SQL statement. Commit as per the requirement.
+     * @param sql
+     * @param doCommit
+     * @throws SQLException
+     */
+    public void executeSql(String sql, boolean doCommit) throws SQLException {
+        executeSql(sql, doCommit, AppConstants.DEFAULT_FETCH_SIZE, ResultSet.FETCH_FORWARD);
+    }
+    /**
      * Utilizing the open DB Connection, execute the given SQL.
      * If needed, execute a commit.
      *
@@ -85,15 +127,9 @@ public class DbConnection {
      * @throws SQLException In case of any errors while execution.
      */
     public void executeSql(String sql, boolean doCommit, int fetchSize, int fetchDirn) throws SQLException {
-        if (connection == null) {
-            LOGGER.error("DB: Uninitialized DB Connection.");
-            throw new AppException("Uninitialized DB Connection.");
-        }
 
-        if (sql == null || sql.trim().isEmpty()) {
-            LOGGER.error("DB : Cannot execute null or empty SQL.");
-            throw new IllegalArgumentException("Null or empty SQL.");
-        }
+        connectionCheck();
+        blankStringCheck(sql, "DB : Cannot execute null or empty SQL.");
 
         LOGGER.info("DB : Executing the SQL : {}", sql);
 
@@ -107,6 +143,39 @@ public class DbConnection {
             LOGGER.info("DB : Committing the changes.");
             connection.commit();
         }
+    }
+
+    /**
+     * Fetch ResultSet from DB Connection.
+     *
+     * @param sql   SELECT statement to be executed
+     * @return      {@link ResultSet} containing the information fetched using given SQL.
+     * @throws SQLException
+     */
+    public ResultSet fetchResult(String sql) throws SQLException {
+        connectionCheck();
+        blankStringCheck(sql, "DB : Cannot execute blank SQL.");
+        Statement stmt = connection.createStatement();
+        return executeFetch(sql, AppConstants.DEFAULT_FETCH_SIZE, ResultSet.FETCH_FORWARD);
+    }
+
+    /**
+     * Using the given fetchSize and fetchDirection, get the ResultSet.
+     *
+     * @param sql       SELECT statement to be executed
+     * @param fetchSize Size of the batch of rows to be fetched in one go.
+     * @param fetchDirn Direction of fetch.
+     * @return
+     * @throws SQLException
+     */
+    public ResultSet executeFetch(String sql, int fetchSize, int fetchDirn) throws SQLException {
+        connectionCheck();
+        blankStringCheck(sql, "DB : Cannot execute blank SQL.");
+
+        Statement stmt = connection.prepareStatement(sql);
+        stmt.setFetchSize(fetchSize);
+        stmt.setFetchDirection(fetchDirn);
+        return stmt.getResultSet();
     }
 
     /**
@@ -128,6 +197,32 @@ public class DbConnection {
             LOGGER.error("Error closing the connection.", e);
         }
 
+    }
+
+    /**
+     * Check if the connection is initialized. Throw an {@link IllegalArgumentException} if not.
+     */
+    private void connectionCheck() {
+        AppUtils.nullCheck(connection, "DB: Uninitialized DB Connection.");
+    }
+
+    private static class DbCred {
+        String url, user, pwd;
+
+        public DbCred(String url, String user, String pwd) {
+            this.url = url;
+            this.user = user;
+            this.pwd = pwd;
+        }
+
+        @Override
+        public String toString() {
+            return "DbCred{" +
+                    "url='" + url + '\'' +
+                    ", user='" + user + '\'' +
+                    ", pwd='" + pwd + '\'' +
+                    '}';
+        }
     }
 
 }
